e7b9afc7cd5b3001c4e53a32f9c1c45a513e2680
unknown <ahmed.sheha097@gmail.com>
2023-05-15 22:08:09 +0300

    dockerfiles

--- gateway/server.js
+++ gateway/server.js
@@ -6,12 +6,16 @@ const app = express();
 app.use(cors());
 
 app.get("/service/:service", async (req, res) => {
-  const services = ["a", "b"];
-  const service = req.params.service?.toLowerCase();
-  if (!services.includes(service)) return res.send("invalid service");
-  const port = service === "a" ? 4001 : 4002;
-  const apiRes = await axios.get(`http://localhost:${port}`);
-  res.json({ msg: `Service ${apiRes.data.msg}` });
+  try {
+    const services = ["a", "b"];
+    const service = req.params.service?.toLowerCase();
+    if (!services.includes(service)) return res.send("invalid service");
+    const port = service === "a" ? 4001 : 4002;
+    const apiRes = await axios.get(`http://localhost:${port}`);
+    res.json({ msg: `Service ${apiRes.data.msg}` });
+  } catch (err) {
+    res.status(500).json({ msg: "Server Error..." });
+  }
 });
 
 app.listen(4000, () => {
